7789c13d5abd46b3db07ff9bd3038eeb86e1b788
Vivien <vivientopou57@gmail.com>
2023-03-28 11:27:01 +0000

    commit

--- resources/views/frontend/bj/home.blade.php
+++ resources/views/frontend/bj/home.blade.php
@@ -1,92 +1,33 @@
-@include('frontend.bj.header.header')
-@include('frontend.bj.header.header1')
-@include('frontend.bj.header.header2')
-@include('frontend.bj.header.header3')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/slider.css')}}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/side-slider.css')}}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/carousel.css')}}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/video-player.css')}}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/autocompletion.css') }}" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-@include('frontend.bj.header.header4')
-@include('frontend.bj.header.header5')
-@include('frontend.bj.header.header6')
-@include('frontend.bj.header.header7')
-@include('frontend.bj.header.header8')
-@include('frontend.bj.header.header9')
-@include('frontend.bj.navbar')
-
-<div class="container">
-    <img src="{{ asset('assets/images/Annuaire.showroom.africa.png') }}" alt="">
-</div>
-
-@include('frontend.bj.footer.footer')
-<script src="{{ asset('assets/js/script.js') }}"></script>
-@include('frontend.bj.footer.footer1')
-<script src="{{ asset('assets/js/video-modal.js') }}"></script>
-@include('frontend.bj.footer.footer2')
-<script src="{{ asset('assets/js/slider.js') }}"></script>
-<script src="{{ asset('assets/js/autocompletion.js') }}"></script>
-
-<script type="text/javascript">
-    var counter = 1;
-    var count = 1;
-    const sliderInterval = setInterval(sliderTimer, 10000);
-
-    function sliderTimer() {
-        var radio = document.getElementById('rdo' + count);
-        radio.checked = true;
-        count++;
-        if (count > 2) {
-            count = 1;
-        }
-    }
-
-    setInterval(function () {
-        document.getElementById('radio' + counter).checked = true;
-        counter++;
-        if (counter > 2) {
-            counter = 1;
-        }
-    }, 12000);
-
-</script>
-
-<script type="text/javascript">
-
-    var text = document.getElementById("searchfield");
-    var icon = document.getElementById("searchicon");
-    var ai = document.getElementById("annuaire_i");
-    var a = document.getElementById("annuaire");
-
-    function changeText() {
-        text.placeholder = "Rechercher avec un numéro de téléphone";
-        ai.classList.add("btn-active");
-        a.classList.remove("btn-active");
-        icon.classList.remove("fa-buildings");
-        icon.classList.add("fa-phone");
-    }
-    function resetText() {
-        text.placeholder = "Rechercher une entreprise ou un professionnel";
-        a.classList.add("btn-active");
-        ai.classList.remove("btn-active");
-        icon.classList.remove("fa-phone");
-        icon.classList.add("fa-buildings");
-    }
-
-    /* Popup */
-    var pop = document.getElementById("popup");
-    var cp = document.getElementById("closepop");
-
-    function loadPopup() {
-        pop.style.display = "block";
-    }
-
-    cp.onclick = function () {
-        pop.style.display = "none";
-    }
-       /* End Popup */
-
-</script>
-@include('frontend.bj.footer.footer3')
+    <link rel="stylesheet" href="{{ asset('assets/css/lanching_style.css') }}">
+    <title>Launching Soon</title>
+</head>
+<body>
+    <div class="container">
+        <div class="head">
+            <span class="an">ANNUAIRE</span>
+            <span>SHOWROOM</span>
+            <span class="af">AFRICA</span>
+        </div>
+        <div class="body">
+            <span>Launching</span>
+            <span class="so">soon!</span>
+            <p>We are currently making some improvements to our website!</p>
+        </div>
+        <div class="box">
+            <p>NOTIFY ME</p>
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-twitter"></i>
+        </div>
+    </div>
+</body>
+</html>
