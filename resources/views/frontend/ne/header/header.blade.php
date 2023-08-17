c436dd5ae79841c453fcb975df2b18c6e1d3b106
Vivien <vivientopou57@gmail.com>
2023-08-17 15:20:12 +0000

    commit

--- resources/views/frontend/ne/header/header.blade.php
+++ resources/views/frontend/ne/header/header.blade.php
@@ -3,13 +3,68 @@
 
 <head>
     <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    
+    <meta http-equiv="Content-Language" content="fr-NE" />
+    <meta name="robots" content="index, follow">
+    <meta name="geo.country" content="NE" />
+    <meta name="country" content="NE" />
+    <meta name="description" content="<strong>Annuaire</strong> des <strong>entreprises</strong> et des <strong>professionnels</strong> en réseau pour une émergence <strong>économique</strong> du Togo et de l’Afrique. Guide pour <strong>l’investissement</strong>,
+    l’annuaire showroom africa est une liste des sociétés répertoriées par secteurs d’activités. Pour faciliter les recherches, showroom africa positionne votre entreprise dans votre secteur et votre activité se retrouve booster .">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="theme-color" content="#6777ef"/>
-    <title>Showroom Africa - Votre annuaire</title>
+    <meta name="keyword" content="annuaire showroom africa, annuaire des entreprises, répertoire des professionnels, service, devis, offre, emploi ..." />
+    <meta property="og:title" content="Annuaire Showroom Africa" />
+    <meta property="og:image" content="https://showroomafrica.com/assets/images/showroom/logo.png" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Showroom Africa" />
+    <meta property="og:locale" content="fr-NE" />
+    <meta property="og:url" content="https://showroomafrica.com/ne/11" />
+    <meta property="og:country-name" content="Niger" />
+    <meta property="og:description" content="<strong>Annuaire</strong> des <strong>entreprises</strong> et des <strong>professionnels</strong> en réseau pour une émergence <strong>économique</strong> du Togo et de l’Afrique. Guide pour <strong>l’investissement</strong>,
+    l’annuaire showroom africa est une liste des sociétés répertoriées par secteurs d’activités. Pour faciliter les recherches, showroom africa positionne votre entreprise dans votre secteur et votre activité se retrouve booster ." />
+    
+    
+    
+    <title>Annuaire Showroom Africa</title>
     <!-- FAVICON -->
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/showroom/favicon.ico') }}" />
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
+    <link rel="canonicail" href="https://showroomafrica.com/ne/11">
+
+    <link rel="alternate" href="https://showroomafrica.com/annuaire" hreflang="fr"/>
+    
+    <link rel="alternate" href="https://showroomafrica.com/bj/1" hreflang="fr-BJ"/>
+    <link rel="alternate" href="https://showroomafrica.com/bf/2" hreflang="fr-CI"/>
+    <link rel="alternate" href="https://showroomafrica.com/bi/21" hreflang="fr-BI"/>
+    <link rel="alternate" href="https://showroomafrica.com/cm/3" hreflang="fr-CM"/>
+    <link rel="alternate" href="https://showroomafrica.com/cf/4" hreflang="fr-CF"/>
+    <link rel="alternate" href="https://showroomafrica.com/cg/5" hreflang="fr-CG"/>
+    <link rel="alternate" href="https://showroomafrica.com/ci/6" hreflang="fr-CI"/>
+    <link rel="alternate" href="https://showroomafrica.com/dj/22" hreflang="fr-DJ"/>
+    <link rel="alternate" href="https://showroomafrica.com/ga/4" hreflang="fr-GA"/>
+    <link rel="alternate" href="https://showroomafrica.com/gn/26" hreflang="fr-GN"/>
+    <link rel="alternate" href="https://showroomafrica.com/mg/23" hreflang="fr-MG"/>
+    <link rel="alternate" href="https://showroomafrica.com/ml/10" hreflang="fr-ML"/>
+    <link rel="alternate" href="https://showroomafrica.com/mr/24" hreflang="fr-MR"/>
+    <link rel="alternate" href="https://showroomafrica.com/cd/15" hreflang="fr-CD"/>
+    <link rel="alternate" href="https://showroomafrica.com/rw/25" hreflang="fr-RW"/>
+    <link rel="alternate" href="https://showroomafrica.com/sn/12" hreflang="fr-SN"/>
+    <link rel="alternate" href="https://showroomafrica.com/td/13" hreflang="fr-TD"/>
+    <link rel="alternate" href="https://showroomafrica.com/tg/14" hreflang="fr-TG"/>
+
+
+
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WKGQRXLQ4V"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', 'G-WKGQRXLQ4V');
+    </script>
 
     
 
